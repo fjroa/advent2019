e688ff8baa7a4a4b948be1465e51f5733b01a6a1
Francisco Javier Roa Lopez <fcojavier.roa@bestsecret.com>
2019-12-10 21:47:41 +0100

    Initial commit

---
+++ src/main/kotlin/com/fjroa/advent2019/Advent2019Application.kt
@@ -0,0 +1,11 @@
+package com.fjroa.advent2019
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Advent2019Application
+
+fun main(args: Array<String>) {
+    runApplication<Advent2019Application>(*args)
+}
---
+++ src/test/kotlin/com/fjroa/advent2019/Advent2019ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fjroa.advent2019
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Advent2019ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
